c9687f725dce6e1a086c8e5d2e2f665609259a22
Jamey Geoghagan <Jameygeoghagan@gmail.com>
2020-07-27 15:06:23 -0500

    Made the prgram and it is running great!

---
+++ WhenTwoTalk/Program.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO.Pipes;
+using System.Net.Http;
+
+namespace WhenTwoTalk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            bool toDO = true;
+            do
+            {
+                QuoteGenerater.KanyeQuote();
+                QuoteGenerater.RonQuote();
+                Console.WriteLine("Keep The Convo going?  Y/N");
+                var ans = Console.ReadLine();
+                var answer = ans.ToLower();
+                if (answer == "y")
+                {
+                    toDO = true;
+                }
+                else { toDO = false; }
+            } while (toDO == true);
+
+              
+        }
+    }
+}
